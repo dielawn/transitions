e10b50fa127c462a93eda8fb6a4c4f08fe847806
dielawn <dielawnmerkill@gmail.com>
2023-11-25 05:34:41 -0700

    transition tutorial first lesson

---
+++ script.js
@@ -0,0 +1,6 @@
+const containerDiv = document.getElementById('container')
+
+const transitionDiv = document.createElement('div')
+transitionDiv.classList.add('transition')
+containerDiv.appendChild(transitionDiv)
+
